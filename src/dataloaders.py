d567dee67b6ac383282c524f2df30cab63a0f19c
loganoneal <loganofneal@gmail.com>
2024-04-10 18:53:30 -0400

    zones buildings

--- src/dataloaders.py
+++ src/dataloaders.py
@@ -7,3 +7,9 @@ def load_zone_info(file_path):
     min_widths = zones["min_width"].to_numpy()
 
     return type, min_areas, min_widths
+
+def load_building_info(file_path):
+    building = pd.read_csv(file_path)
+    W, L = building.to_numpy()[0]
+
+    return W, L
--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@ import pandas as pd
     
 from dataloaders import load_zone_info
 
-W, L = 100, 100
+W, L = 1000, 1000
 
 type, min_areas, min_widths = load_zone_info("./data/zones.csv")
+
 
 
 def main():
