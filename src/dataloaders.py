5dbcc8489f5ff02d93bc2bee82a0bceaadacbe2b
loganoneal <loganofneal@gmail.com>
2024-04-14 19:39:30 -0400

    new chromosome layout, zone scoring

--- src/dataloaders.py
+++ src/dataloaders.py
@@ -4,20 +4,13 @@ import networkx as nx
 
 def load_zone_info(file_path):
     zones = pd.read_csv(file_path)
-    types = zones["type"].to_numpy()
-    min_areas = zones["min_area"].to_numpy()
-    min_widths = zones["min_width"].to_numpy()
 
-    return types, min_areas, min_widths
+    return zones
 
 def load_building_info(file_path):
-    buildings = pd.read_csv(file_path)
-    types = buildings["type"].to_numpy()
-    min_areas = buildings["min_area"].to_numpy()
-    min_widths = buildings["min_width"].to_numpy()
-    required_zone = buildings["required_zone"].to_numpy()
+    buildings_df = pd.read_csv(file_path)
 
-    return types, min_areas, min_widths, required_zone, len(buildings)
+    return buildings_df
 
 def load_relationship_graph(N, file_path):
     relationships_df = pd.read_csv(file_path, header='infer')
--- src/initialization.py
+++ src/initialization.py
@@ -1,6 +1,6 @@
 import numpy as np
 import networkx as nx
-
+from constants import ZONE_W, ZONE_H
 
 def initialize_population(sol_per_pop, N, relationship_graph):  # TODO - Randomly switch directions
     initial_population = []
@@ -15,6 +15,39 @@ def initialize_population(sol_per_pop, N, relationship_graph):  # TODO - Randoml
         initial_population.append(chromosome)
     return initial_population
 
+def initialize_population(sol_per_pop, buildings_df, zones_df, W, H):
+    print("Initializing population")
+    # create a population where each chronosome is a W * H grid of buildings with random positions
+    min_area = W*H
+    if min_area < len(buildings_df):
+        raise ValueError("The area of the community is too small for the number of buildings")
+
+    initial_population = []
+    for _ in range(sol_per_pop):
+        chromosome = np.zeros((W, H))
+        # randomly place buildings inside the chromosome
+        for i in range(W):
+            for j in range(H):
+                # get random building 
+                building = buildings_df.sample()
+                # set chromosme indx to building id
+                chromosome[i, j] = building["id"]
+
+        chromosome = chromosome.flatten()
+        initial_population.append(chromosome)
+        
+    zones = np.zeros((W, H))
+    # cluster zones together into 2x2 sections
+    for i in range(W // ZONE_W):
+        for j in range(H // ZONE_H):
+            # get random zone
+            zone = zones_df.sample()
+            # set the section to the zone id 
+            zones[i * ZONE_W:(i + 1) * ZONE_W, j * ZONE_H:(j + 1) * ZONE_H] = zone["id"] 
+    zones = zones.flatten()
+                    
+    return initial_population, zones            
+        
 
 def get_fruchterman_reingold_relative_positionings(G):
     pos = nx.spring_layout(G)
