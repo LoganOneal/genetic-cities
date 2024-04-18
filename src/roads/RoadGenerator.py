ad90641f1dc88abcec1105aed72a42085857c9d8
Caleb Fisher <cfishe36@vols.utk.edu>
2024-04-17 22:06:28 -0400

    initial version of road network generator

---
+++ src/roads/RoadGenerator.py
@@ -0,0 +1,60 @@
+from matplotlib import pyplot as plt
+from roads.QuadTree import QuadTree
+from roads import Util
+from roads import globals
+import numpy as np
+from random import randrange
+
+class RoadGenerator(object):
+    def __init__(self, size, max_nodes) -> None:
+        self.size = size
+        self.max_nodes = max_nodes
+        self.QT = None
+        
+        globals.init()
+
+    def randomize(self):
+        X=Util.getPlane(self.size)
+
+        mins = (0.0, 0.0)
+        maxs = (self.size-1.0, self.size-1.0)
+
+        self.QT = QuadTree(X, mins, maxs, 0,0)
+
+        self.QT.add_square()
+
+        # for high density choose ones counter depth with highest number of squares randomly
+        while(True):
+            node=randrange(max(globals.node_index))
+            if len(globals.node_index) > self.max_nodes: # limit network generation by number of nodes
+                break
+
+            Util.add_square_at(self.QT,node)
+
+    def print(self):
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.add_subplot(111)
+        ax.set_xlim(0, self.size-1.0)
+        ax.set_ylim(0, self.size-1.0)
+
+        # for each depth generate squares
+        for d in range(0,len(globals.node_index)):
+            self.QT.draw_rectangle(ax, depth=d)
+
+        print ("writing data to files...")
+        fn = open('../node-list','w')
+        fe = open('../edge-list','w')
+
+        edgeCount=0 #directed edge count
+        for point in globals.edges:
+            if point in globals.coord_id:
+                fn.write(str(globals.coord_id[point])+","+str(point.x)+","+str(point.y)+"\n")
+
+            for edge in globals.edges[point]:
+                fe.write(str(globals.coord_id[point])+","+str(globals.coord_id[edge])+"\n")
+                edgeCount=edgeCount+1
+
+        fn.close()
+        fe.close()
+
+        plt.savefig('../images/rand-quad-road-network.png')
---
+++ src/roads/Util.py
@@ -0,0 +1,80 @@
+import numpy as np
+from queue import Queue
+import itertools, random
+from roads import Point
+
+def getPlane(size):
+    X=[]
+    plane=range(0,size)
+    for i in itertools.permutations(plane,2):
+        X.append(i)
+
+    for i in plane:
+        X.append((i,i))
+
+    X=np.array(X)
+
+    return X
+
+def bfs_print(root):
+    print ("*** bfs print ***")
+    q = Queue()
+    s_node=root
+    q.put(s_node)
+    seen_list = []
+
+    while(not q.empty()):
+        node=q.get() # removes
+        print (node.n_node_id)
+        if(node not in seen_list):
+            seen_list.append(node)
+        for child in node.children:
+            if(child not in seen_list):
+                q.put(child)
+    print ("---------------")
+
+def add_square_at(root,nodeid):
+    q = Queue()
+    s_node=root
+    q.put(s_node)
+    seen_list = []
+
+    while(not q.empty()):
+        node=q.get() # removes
+        if nodeid==node.n_node_id:
+            node.add_square()
+        else:
+            if(node not in seen_list):
+                seen_list.append(node)
+            for child in node.children:
+                if(child not in seen_list):
+                    q.put(child)
+
+def printStats(nodeIndex):
+    print ("*** Tree Stats ***")
+    print (nodeIndex)
+
+def getCoordinates(baseXY,boxSize):
+	p1=Point(baseXY[0],baseXY[1])
+	p2=Point(baseXY[0]+boxSize[0],baseXY[1])
+	p3=Point(baseXY[0],baseXY[1]+boxSize[1])
+	p4=Point(p2.x,p2.y+boxSize[1])
+	
+	return (p1,p2,p3,p4)
+	
+
+def getBetaInt(alpha,beta,m,maxv):
+    beta=int(np.random.beta(alpha,beta,1)[0]*m)
+    while(beta>maxv):
+        print (beta)
+        beta=int(np.random.beta(alpha,beta,1)[0]*m)
+
+    return beta
+
+
+
+
+
+
+
+
